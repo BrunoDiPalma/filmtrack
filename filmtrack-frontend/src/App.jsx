eb83a534c3dd6ebb5e413761100ce7a0eb2de346
BrunoDiPalma <brunocamargo2010@live.com>
2026-04-02 17:31:58 -0300

    feat: implementar busca de filmes com API externa e gerenciamento de wishlist

--- filmtrack-frontend/src/App.jsx
+++ filmtrack-frontend/src/App.jsx
@@ -1,27 +1,83 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import { MovieList } from "./components/MovieList";
+import { MovieCard } from "./components/MovieCard";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch("http://localhost:3000");
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000`);
       const data = await response.json();
       setMovies(data.data);
+    } catch (error) {
+      console.error("Erro ao buscar filmes", error);
     }
+  };
+
+  useEffect(() => {
     fetchMovies();
   }, []);
 
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`http://localhost:3000/${id}`, {
+        method: "DELETE",
+      });
+      setMovies((prevMovies) => prevMovies.filter((movie) => movie.id !== id));
+    } catch (error) {
+      console.error("Erro ao excluir filme da lista", error);
+    }
+  };
+
+  const handleSearch = async () => {
+    if (!searchTerm.trim()) return;
+
+    const response = await fetch(
+      `http://www.omdbapi.com/?s=${searchTerm}&apikey=fa7cf79e`,
+    );
+
+    const data = await response.json();
+
+    if (data.Search) {
+      setSearchResults(data.Search);
+    } else {
+      setSearchResults([]);
+    }
+  };
+
+  const handleSave = async (movie) => {
+    const newMovie = {
+      title: movie.Title,
+      year: movie.Year,
+    };
+
+    await fetch(`http://localhost:3000`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMovie),
+    });
+    fetchMovies();
+  };
+
   return (
     <div>
-      <h1>Filmtrack! 🎥</h1>
-      {movies.map((movie) => (
-        <div key={movie.id}>
-          <h2>{movie.title}</h2>
-          <p>{movie.genre}/</p>
-        </div>
+      <h1>FilmTrack! 🎥</h1>
+      <input
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Buscar</button>
+
+      {searchResults.map((movie) => (
+        <MovieCard key={movie.imdbID} movie={movie} onSave={handleSave} />
       ))}
+      <MovieList movies={movies} onDelete={handleDelete} />
     </div>
   );
 }
